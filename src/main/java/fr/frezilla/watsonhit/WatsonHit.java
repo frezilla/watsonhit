74e8dac703e3128afaf6a76d4116fef44eaa0dbe
frezilla <>
2019-11-05 17:38:30 +0100

    Modification de la classe principale

--- src/main/java/fr/frezilla/watsonhit/WatsonHit.java
+++ src/main/java/fr/frezilla/watsonhit/WatsonHit.java
@@ -1,46 +1,173 @@
 package fr.frezilla.watsonhit;
 
+import fr.frezilla.watsonhit.business.exceptions.BusinessException;
+import fr.frezilla.watsonhit.business.exceptions.BusinessExceptions;
+import fr.frezilla.watsonhit.reader.file.CsvReader;
 import java.io.File;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
-import org.kohsuke.args4j.ParserProperties;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
+@NoArgsConstructor
 public final class WatsonHit {
     
-    private final PrintStream err;
-    private final List<String> arguments;
+    public static final Logger LOGGER = LogManager.getRootLogger();
     
-    @Option(name="-in", aliases = {"--input-csv-file"}, required = true, usage = "nom du fichier CSV à analyser")
-    private File fileIn;
+    private String fileOut;
+    private Level logLevel;
     
-    @Option(name="-out", aliases = {"--result-file"}, required = true, usage="nom du fichier résultat")
-    private File fileOut;
-    
-    private WatsonHit() {
-        err = System.err;
-        arguments = new ArrayList();
+    private void checkCsvFileIn(String csvFileIn) throws BusinessException {
+        LOGGER.debug("vérification du fichier entrée");
+        File f = new File(csvFileIn);
+        if (!f.exists()) {
+            throw BusinessExceptions.fileInNotFound.getException();
+        }
+        if (!f.isFile()) {
+            throw BusinessExceptions.fileInNotValid.getException();
+        }
     }
-    
+
+    private void doMain(String[] args) {
+        try {
+            Options CsvReaderOptions = createCsvReaderOptions();
+            
+            CsvReaderArgs csvReaderArgs = parseCsvReaderArgs(args);
+            CsvReader csvReader = createCSVReader(csvReaderArgs);
+            while (csvReader.hasNext()) {
+                String[] columns = csvReader.next();
+                for (String c : columns) {
+                    System.out.print(c);
+                }
+                System.out.println();
+            }
+            csvReader.close();
+        } catch (ParseException | IOException e) {
+            LOGGER.fatal(e.getMessage());
+        } 
+    }
+
     public static void main(String[] args) {
         new WatsonHit().doMain(args);
     }
-    
-    private void doMain(String[] args) {
-        CmdLineParser parser = new CmdLineParser(this);
-        ParserProperties parserProperties = ParserProperties.defaults();
-        parserProperties.withUsageWidth(80);
+
+    private Map<String, Object> parseArguments(@NonNull String[] args) throws ParseException {
+        Options options = new Options();
         
-        try {
-            parser.parseArgument(args);
-        } catch (CmdLineException e) {
-            err.println(e.getMessage());
-            err.println("java WatsonHist [");
-            parser.printUsage(err);
-            err.println();
+        options.addOption("d", "debug", false, "activation du mode debug");
+        options.addOption("del", "csv-delimiter", true, "délimiteur de zones du fichier csv");
+        options.addRequiredOption("in", "csv-file-in", true, "fichier csv à traiter");
+        options.addRequiredOption("out", "file-out", true, "fichier résultat");
+        
+        OptionGroup optionGroup = new OptionGroup();
+        optionGroup.addOption(new Option("ctlsize", "csv-delimiter", false, "active le contrôle du nombre de colonnes du fichier csv"));
+        optionGroup.addOption(new Option("wh", "csv-with-header", false, "la première ligne du fichier csv est une ligne de titre"));
+        options.addOptionGroup(optionGroup);
+        
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        logLevel = (cmd.hasOption("d") ? Level.DEBUG : Level.INFO);
+        
+        fileOut = cmd.getOptionValue("out");
+        
+        CsvReaderArgs csvReaderArgs = new CsvReaderArgs();
+        csvReaderArgs.delimiter = (cmd.hasOption("del") ? cmd.getOptionValue("del") : CsvReader.DEFAULT_DELIMITER);
+        csvReaderArgs.fileIn = cmd.getOptionValue("in");
+        csvReaderArgs.withHeader = cmd.hasOption("wh");
+        
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("csvReaderArguments", csvReaderArgs);
+        
+        return arguments;
+    }
+
+    private CsvReader createCSVReader(CsvReaderArgs args) throws FileNotFoundException {
+        LOGGER.debug(String.format("initialisation du traitement de lecture du fichier csv\n"
+                + "%s", args.toString()));
+        CsvReader.Builder builder = new CsvReader.Builder(args.fileIn);
+        if (args.controlColumnsSize) {
+            builder.controlColumnsSize();
         }
-    }    
+        builder.setDelimiter(args.delimiter);
+        if (args.withHeader) {
+            builder.withHeader();
+        }
+        return builder.build();
+    }
+
+    private Options createCsvReaderOptions() {
+        Options options = new Options();
+        
+        return options;
+    }
+
+    private CsvReaderArgs parseCsvReaderArgs(String[] args) {
+        Options options = new Options();
+        
+        options.addOption(Option.builder("csv-delimiter").hasArg(true).desc("délimiteur de zones du fichier csv").build());
+        options.addOption(Option.builder("csv-filename").hasArg(true).desc("nom du fichier csv à traiter").build());
+        
+        options.addOption(Option.builder("csv-withheader").hasArg(false).desc("la première ligne du fichier csv est une ligne de titre").build());
+
+        options.addOption("d", "debug", false, "activation du mode debug");
+        options.addOption("del", "csv-delimiter", true, "délimiteur de zones du fichier csv");
+        options.addRequiredOption("in", "csv-file-in", true, "fichier csv à traiter");
+        options.addRequiredOption("out", "file-out", true, "fichier résultat");
+        
+        OptionGroup optionGroup = new OptionGroup();
+        optionGroup.addOption(new Option("ctlsize", "csv-delimiter", false, "active le contrôle du nombre de colonnes du fichier csv"));
+        optionGroup.addOption(new Option("wh", "csv-with-header", false, "la première ligne du fichier csv est une ligne de titre"));
+        options.addOptionGroup(optionGroup);
+        
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        logLevel = (cmd.hasOption("d") ? Level.DEBUG : Level.INFO);
+        
+        fileOut = cmd.getOptionValue("out");
+        
+        CsvReaderArgs csvReaderArgs = new CsvReaderArgs();
+        csvReaderArgs.delimiter = (cmd.hasOption("del") ? cmd.getOptionValue("del") : CsvReader.DEFAULT_DELIMITER);
+        csvReaderArgs.fileIn = cmd.getOptionValue("in");
+        csvReaderArgs.withHeader = cmd.hasOption("wh");
+        
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("csvReaderArguments", csvReaderArgs);
+        
+        return arguments;        
+    }
+    
+    private static class CsvReaderArgs {
+        private boolean controlColumnsSize;
+        private String delimiter;
+        private String fileIn;
+        private boolean withHeader; 
+        
+        @Override
+        public String toString() {
+            return String.format("-> nom du fichier : <%s>\n"
+                    + "-> délimiteur : <%s>\n"
+                    + "-> entêtes de colonnes incluses : <%s>\n"
+                    + "-> contrôle du nombre de colonnes : <%s>", 
+                    fileIn,
+                    delimiter,
+                    withHeader ? "oui" : "non",
+                    controlColumnsSize ? "oui" : "non");
+        }
+    }
+
 }
---
+++ src/main/java/fr/frezilla/watsonhit/business/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package fr.frezilla.watsonhit.business.exceptions;
+
+public class BusinessException extends Exception {
+
+    BusinessException(String message) {
+        super(message);
+    }
+    
+}
